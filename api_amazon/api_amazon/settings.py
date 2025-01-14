497b86303de94b82215b762ab5a761c9b18770e1
Victor Paiva Passos <20231014040042@ifrn.local>
2025-01-14 11:10:36 -0300

    :wrench: chore: plugin de autenticação simplejwt instalado

--- api_amazon/api_amazon/settings.py
+++ api_amazon/api_amazon/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
