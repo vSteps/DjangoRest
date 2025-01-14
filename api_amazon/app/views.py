19f3fec7f8cd3dde54e7c4dc9368d3095d4d5402
Victor Paiva Passos <20231014040042@ifrn.local>
2025-01-14 11:13:49 -0300

    :sparkles: feat: views de login e signup com token do usuário feitas

--- api_amazon/app/views.py
+++ api_amazon/app/views.py
@@ -1,5 +1,13 @@
+## Permissions
+from rest_framework.decorators import authentication_classes, permission_classes # type: ignore
+from rest_framework.permissions import IsAuthenticated  # type: ignore
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication # type: ignore
+from django.shortcuts import get_object_or_404 # type: ignore
+
+
 from rest_framework.decorators import api_view
 from rest_framework import  viewsets
+
 
 from app.models import  Cliente, Endereco, Pedido, Item, Vendedor, Forma_Pagamento
 from app.serializers import  ClienteSerializer, EnderecoSerializer, PedidoSerializer, ItemSerializer, VendedorSerializer, Forma_PagamentoSerializer
@@ -7,6 +15,34 @@ from app.serializers import  ClienteSerializer, EnderecoSerializer, PedidoSerial
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer 
+
+    @api_view(['POST'])
+    def login(request):
+        user = get_object_or_404(User, username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response({'message': 'Not Found!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = MeuUsuarioSerializer(instance=user)
+        return Response({'token': token.key, 'user':serializer.data})
+
+    @api_view(['POST'])
+    def signup(request):
+        serializer = MeuUsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.get(username=request.data['username'])
+            user.set_password(request.data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key, 'user':serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @api_view(['GET'])
+    @authentication_classes([TokenAuthentication, SessionAuthentication])
+    @permission_classes([IsAuthenticated])
+    def test_token(request):
+        return Response("passou para {}".format(request.user.email))
 
 class EnderecoViewSet(viewsets.ModelViewSet):
     queryset = Endereco.objects.all()
